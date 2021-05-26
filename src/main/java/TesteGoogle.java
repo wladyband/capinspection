79d86da84070361a68b8bec68b0ae05aeff15883
Wladimir Bandeira <wladimir.bandeira@capgemini.com>
2021-05-26 17:31:59 -0300

    criação de estrutura básica de um projeto em Selenium

---
+++ src/main/java/TesteGoogle.java
@@ -0,0 +1,16 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TesteGoogle {
+	
+	@Test
+	public void teste() {
+		System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com.br");
+		Assert.assertEquals("Google", driver.getTitle());
+	}
+
+}
