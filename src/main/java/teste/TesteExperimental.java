745893fb1b49371b351c7ac7b3e1ee6fbfacf95f
Wladimir Bandeira <wladimir.bandeira@capgemini.com>
2021-05-27 11:02:05 -0300

    otimizando driver do google chrome

--- src/main/java/teste/TesteExperimental.java
+++ src/main/java/teste/TesteExperimental.java
@@ -1,22 +1,35 @@
 package teste;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TesteExperimental {
+
+	
+	private WebDriver driver;
+	
+	@Before
+	public void inicializarTestes() {
+		System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://gtc02.tef.intra.rs.gov.br/pagina-inicial");
+	}
+	
+	@After
+	public void finalizarTestes() {
+		driver.quit();
+	}
+	
 	
 	@Test
 	public void deveEntrarLoginCidadao() {
-		System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.get("http://gtc02.tef.intra.rs.gov.br/pagina-inicial");
 		driver.findElement(By.xpath("//button[@class='btn btn-primary']")).click();
 		Assert.assertEquals("Já tenho cadastro no Login Cidadão", driver.findElement(By.className("title")).getText());
-		driver.quit();
 	}
 
-	
 }
