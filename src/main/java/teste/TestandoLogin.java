6aeb47668dccf5a7a0cf37cd21bd192c55f1e1ec
Wladimir Bandeira <wladimir.bandeira@capgemini.com>
2021-05-28 08:20:24 -0300

    organizando melhor a estrutura do projeto

---
+++ src/main/java/teste/TestandoLogin.java
@@ -0,0 +1,41 @@
+package teste;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import br.com.capinspection.core.DSL;
+
+public class TestandoLogin {
+
+	private WebDriver driver;
+	private DSL dsl;
+
+	@Before
+	public void inicializarTestes() {
+		System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://gtc02.tef.intra.rs.gov.br/pagina-inicial");
+		dsl = new DSL(driver);
+	}
+
+	@After
+	public void finalizarTestes() {
+		driver.quit();
+	}
+
+	@Test
+	public void deveEntrarLoginCidadao() throws InterruptedException {
+		dsl.clicarButtonXpath("//button[@class='btn btn-primary']");
+		Assert.assertEquals("Já tenho cadastro no Login Cidadão", dsl.obterTextoPelaTag("title"));
+		dsl.escreverPorXpath("//input[@id='login_form_type_username']", "727.585.220-98");
+		Assert.assertEquals("727.585.220-98", dsl.obterTextoPorXpath("//input[@id='login_form_type_username']"));
+		dsl.clicarButtonXpath("(//button)[1][@class='btn btn-block btn-success']");
+		Thread.sleep(300);
+		dsl.escrever("login_form_type_password", "PROCERGSRU1");
+		Assert.assertEquals("PROCERGSRU1", dsl.obterTextoPorXpath("//input[@id='login_form_type_password']"));
+	}
+}
