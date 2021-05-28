3805db09820a3d00d048614631ba2113d542a096
Wladimir Bandeira <wladimir.bandeira@capgemini.com>
2021-05-28 12:11:16 -0300

    implementando chaveador de browser

--- src/main/java/br/com/capinspection/core/DriverFactory.java
+++ src/main/java/br/com/capinspection/core/DriverFactory.java
@@ -2,6 +2,8 @@ package br.com.capinspection.core;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class DriverFactory {
 
@@ -12,8 +14,20 @@ public class DriverFactory {
 
 	public static WebDriver getDriver() {
 		if (driver == null) {
-			System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
-			driver = new ChromeDriver();
+			switch (Propriedades.browser) {
+			case FIREFOX:
+				System.setProperty("webdriver.gecko.driver", "src/main/resources/drivers/geckodriver.exe");
+				driver = new FirefoxDriver();
+				break;
+			case CHROME:
+				System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
+				driver = new ChromeDriver();
+				break;
+			case IE:
+				System.setProperty("webdriver.ie.driver", "src/main/resources/drivers/IEDriverServer.exe");
+				driver = new InternetExplorerDriver();
+				break;
+			}
 		}
 		return driver;
 	}
